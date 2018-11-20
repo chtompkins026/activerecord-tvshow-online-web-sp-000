949b7fb51557e08bccae486070e49a86d9461716
Christopher Tompkins <chtompkins026@gmail.com>
2018-11-20 23:06:41 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,6 +3,10 @@ class Show < ActiveRecord::Base  #allow us to mess around using SQL <> Ruby
   def self.highest_rating 
     Show.maximum(:rating)
   end 
+  
+  def self.most_popular_show 
+    Show.where('rating')
+  end 
 
 
 end 
