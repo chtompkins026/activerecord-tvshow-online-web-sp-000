82746dfccd2197f309dda047ee16a8312d0edd7b
Christopher Tompkins <chtompkins026@gmail.com>
2018-11-20 22:49:38 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,9 +1,11 @@
 class CreateShows < ActiveRecord::Migration 
-create_table(:shows) do |t| 
-  t.string :name
-  t.string :network 
-  t.string :day 
-  t.integer :rating 
+  
+  create_table(:shows) do |t| 
+    t.string :name
+    t.string :network 
+    t.string :day 
+    t.integer :rating 
+  end 
 end 
 
 #symbols are immutable and unique 
